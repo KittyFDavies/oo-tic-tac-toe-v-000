4b4e578ee44003e918b1b1332f45100446234537
Kitty Davies <kittyfdavies@gmail.com>
2018-03-07 01:37:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,6 +115,16 @@ class TicTacToe
     end
   end
 
-
+  #PLAY
+  def play
+    while over? == false
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 
 end
